f2bad37b306d231279c33ab501fdb4fae7c57d12
Anaelle Jolivet <anaelle0jolivet@gmail.com>
2023-02-01 18:00:25 +0100

    « GET_WORLD » et world.ts

--- frontcapitalist/src/App.tsx
+++ frontcapitalist/src/App.tsx
@@ -1,9 +1,83 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { gql } from '@apollo/client';
+
+const GET_WORLD = gql`
+    query getWorld {
+      getWorld {
+        activeangels
+        allunlocks {
+          idcible
+          logo
+          name
+          ratio
+          seuil
+          typeratio
+          unlocked
+        }
+        angelbonus
+        angelupgrades {
+          idcible
+          logo
+          name
+          ratio
+          seuil
+          typeratio
+          unlocked
+        }
+        lastupdate
+        logo
+        managers {
+          idcible
+          logo
+          name
+          ratio
+          seuil
+          typeratio
+          unlocked
+        }
+        money
+        name
+        products {
+          cout
+          id
+        croissance
+          logo
+          managerUnlocked
+          name
+          paliers {
+            idcible
+            logo
+            name
+            ratio
+            seuil
+            typeratio
+            unlocked
+          }
+          quantite
+          revenu
+          timeleft
+          vitesse
+        }
+        score
+        totalangels
+        upgrades {
+          idcible
+          logo
+          name
+          ratio
+          seuil
+          typeratio
+          unlocked
+        }
+      }
+    }
+  `;
 
 function App() {
   return (
+    
     <div className="App">
       <div className="header">
         <div> logo monde </div> 
