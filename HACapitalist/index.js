fefd77193b9eb03aecf87121827cc27e75294f5d
Anaelle Jolivet <anaelle0jolivet@gmail.com>
2023-02-09 17:02:38 +0100

    mutation

--- HACapitalist/index.js
+++ HACapitalist/index.js
@@ -1,39 +1,39 @@
-const express = require('express');
-let world = require("./world")
+const express = require("express");
+let world = require("./world");
 
-const { ApolloServer } = require('apollo-server-express');
+const { ApolloServer } = require("apollo-server-express");
 // Construct a schema, using GraphQL schema language
 
-
-const typeDefs = require("./schema")
+const typeDefs = require("./schema");
 
 // Provide resolver functions for your schema fields
-const resolvers = require("./resolvers")
+const resolvers = require("./resolvers");
 
 async function readUserWorld(user) {
-    try {
-const data = await fs.readFile("userworlds/"+ user + "- world.json");
-        return JSON.parse(data);
-}
-    catch(error) {
-        return world
-    }
+  try {
+    const fs = require("fs").promises;
+    const data = await fs.readFile("userworlds/" + user + "-world.json");
+    return JSON.parse(data);
+  } catch (error) {
+    return world;
+  }
 }
 
-const server = new ApolloServer({ 
-    typeDefs, resolvers,
-    context: async ({ req }) => ({
-        world: await readUserWorld(req.headers["x-user"]),
-        user: req.headers["x-user"]
-    })
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: async ({ req }) => ({
+    world: await readUserWorld(req.headers["x-user"]),
+    user: req.headers["x-user"],
+  }),
 });
 
 const app = express();
-app.use(express.static('public'));
-server.start().then( res => {
-    server.applyMiddleware({app});
+app.use(express.static("public"));
+server.start().then((res) => {
+  server.applyMiddleware({ app });
 
-    app.listen({port: 4000}, () =>
-console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
-); 
-})
+  app.listen({ port: 4000 }, () =>
+    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+  );
+});
--- HACapitalist/resolvers.js
+++ HACapitalist/resolvers.js
@@ -1,6 +1,7 @@
 function saveWorld(context) {
-  const fs = require('fs');
-  fs.writeFile("userworlds/" + context.user + "-world.json",
+  const fs = require("fs").promises;
+  fs.writeFile(
+    "userworlds/" + context.user + "-world.json",
     JSON.stringify(context.world),
     (err) => {
       if (err) {
@@ -10,7 +11,6 @@ function saveWorld(context) {
     }
   );
 }
-
 module.exports = {
   Query: {
     getWorld(parent, args, context) {
@@ -18,5 +18,57 @@ module.exports = {
       return context.world;
     },
   },
-  Mutation: {},
+  Mutation: {
+    acheterQtProduit(parent, args, context) {
+      let world = context.world;
+
+      let produitid = args.id;
+      let produitquantite = args.quantite;
+
+      let produit = world.products.find((p) => p.id === produitid);
+
+      if (produit === undefined) {
+        throw new Error(`Le produit avec l'id ${args.id} n'existe pas`);
+      } else {
+        produit.quantite += produitquantite,
+          world.money -= produit.cout,
+          produit.cout = produit.cout * produit.croissance
+        saveWorld(context);
+      }
+      return produit;
+    },
+    lancerProductionProduit(parent, args, context) {
+      let world = context.world;
+
+      let produitid = args.id;
+
+      let produit = world.products.find((p) => p.id === produitid);
+
+      if (produit === undefined) {
+        throw new Error(`Le produit avec l'id ${args.id} n'existe pas`);
+      } else {
+        (produit.timeleft = produit.vitesse), (world.lastupdate = Date.now());
+        saveWorld(context);
+      }
+      return produit;
+    },
+    engagerManager(parent, args, context) {
+      let world = context.world;
+
+      console.log(args);
+      let managername = args.name;
+
+      let manager = world.managers.find((m) => m.name === managername);
+
+      if (manager === undefined) {
+        throw new Error(`Le manager avec le nom ${args.name} n'existe pas`);
+      } else {
+        let produit = world.products.find((p) => p.id === idcible);
+
+        (produit.managerUnlock = true), (manager.unlocked = true);
+        saveWorld(context);
+      }
+      return produit;
+    },
+  },
 };
